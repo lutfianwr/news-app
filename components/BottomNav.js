dba4717ce17f3e62ac448c42508979b414c3e0fa
lutfi nuril anwar <lutfi.nurilanwar@gmail.com>
2022-11-30 22:16:53 +0700

    add hide onscrol

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -5,23 +5,53 @@ import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import HouseIcon from "@mui/icons-material/House";
 import TurnedInIcon from "@mui/icons-material/TurnedIn";
 import PersonIcon from "@mui/icons-material/Person";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import PropTypes from "prop-types";
+import Slide from "@mui/material/Slide";
 
-export default function SimpleBottomNavigation() {
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="up" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function SimpleBottomNavigation(props) {
   const [value, setValue] = React.useState(0);
 
   return (
     <Box sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction href="." icon={<HouseIcon />} />
-        <BottomNavigationAction icon={<TurnedInIcon />} />
-        <BottomNavigationAction icon={<PersonIcon />} />
-      </BottomNavigation>
+      <HideOnScroll {...props}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction href="." icon={<HouseIcon />} />
+          <BottomNavigationAction icon={<TurnedInIcon />} />
+          <BottomNavigationAction icon={<PersonIcon />} />
+        </BottomNavigation>
+      </HideOnScroll>
     </Box>
   );
 }
